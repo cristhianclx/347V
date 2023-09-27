ee99d5fe1963a75f9f0903580a01a8e23e1faea6
Cristhian <cristhianclx@gmail.com>
2023-09-26 21:58:16 -0500

    update example

--- 03/main.py
+++ 03/main.py
@@ -8,3 +8,31 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+users_data = [{
+    "name": "cristhian",
+    "age": 34,
+    "language": "python",
+}, {
+    "name": "genaro",
+    "age": 34,
+    "language": "nodejs",
+}]
+    
+
+@app.route('/users')
+def users():
+    return render_template("users.html", users=users_data)
+
+@app.route('/users/<name>')
+def users_by_name(name):
+    user = {}
+    for user_data in users_data:
+        if user_data.get("name") == name:
+            user = user_data
+    return render_template("user.html", user=user)
