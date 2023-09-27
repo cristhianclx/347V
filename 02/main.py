243bd570c8dc835ed80f75fb34291ebc851a3bca
Cristhian <cristhianclx@gmail.com>
2023-09-26 21:13:02 -0500

    update 03

--- 02/main.py
+++ 02/main.py
@@ -24,17 +24,15 @@ def read():
     data["text"] = text
     return data
 
+
 # /read/faq.pdf/3
 
-@app.route('/read/<name>/<page>')
-def read_page(name, page):
+@app.route('/read/<name>/<int:page_number>')
+def read_page(name, page_number):
     data = {}
-    reader = PdfReader("data/faq.pdf")
+    reader = PdfReader("data/{}".format(name))
     number_of_pages = len(reader.pages)
-    text = ""
-    for i in range(0, number_of_pages):
-        page = reader.pages[i]
-        text = text + "\n" + page.extract_text()
-    data["pages"] = number_of_pages
-    data["text"] = text
+    page = reader.pages[page_number]
+    data["page"] = page_number
+    data["text"] = page.extract_text()
     return data
