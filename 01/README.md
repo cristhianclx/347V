8182c327af29883068498676323253c9bc7152d3
Cristhian <cristhianclx@gmail.com>
2023-09-26 20:18:33 -0500

    01

---
+++ 01/README.md
@@ -0,0 +1,23 @@
+# 1ra vez
+
+python3 -m venv venv # crea el entorno virtual
+source venv/bin/activate # activa el entorno virtual
+
+pip install Flask
+
+pip install -r requirements.txt
+
+pip install -r requirements.txt --upgrade # instalas los paquetes
+
+pip freeze # que esta instalado
+
+deactivate # desactiva el entorno virtual
+
+# 
+
+source venv/bin/activate # activa el entorno virtual
+pip install -r requirements.txt --upgrade # instalas los paquetes
+
+#
+
+flask --app main run
---
+++ 01/main.py
@@ -0,0 +1,67 @@
+from flask import Flask
+from markupsafe import escape
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/messages/')
+def messages():
+    return []
+
+@app.route('/messages-in-dashboard/')
+def messages_in_dashboard():
+    return []
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+data = [{
+    "user": "cristhian",
+    "password": 123456,
+}]
+
+# GET /dashboard/ => {}
+# POST > {} => view
+# PUT
+# PATCH
+# DELETE
+
+# /login 
+# 
+# application/json
+#
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data_input = request.get_json()
+        user = data_input["user"]
+        password = data_input["password"]
+        if user and password:
+            return "Valid login"
+            # reviar si existe en el diccionario
+        else:
+            return "Invalid login"
+    else:
+        return "You need to login"
