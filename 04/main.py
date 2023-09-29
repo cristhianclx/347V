6ba3cf788626acbc029333fe9f3324a08bb697e8
Cristhian <cristhianclx@gmail.com>
2023-09-28 20:10:35 -0500

    update data

--- 04/main.py
+++ 04/main.py
@@ -1,16 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.sql import func
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@host:port/database_name'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@host:port/database_name'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sistemas@127.0.0.1:5432/cibertec'
 
 db = SQLAlchemy(app)
+app.debug = True
+
 migrate = Migrate(app, db)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150))
+    name = db.Column(db.String(150), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
-# User - age Int, content Text
+    def __repr__(self):
+        return f"<User {self.id}>"
+    
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # content: text
+    # fk: user
